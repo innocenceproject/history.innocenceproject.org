972aff6cc7ea16429be3d88ac2bcf6132a4d1457
madeline-holland <madeline.holland@ten7.com>
2022-08-15 02:44:40 +0000

    Automated build from commit: acbb7110da09a1eaac86e4591feb151e16a9f968

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
-const blocks = document.querySelectorAll('[data-year]');
-const sideTimelineYears = document.querySelectorAll('nav li');
-const yearsArray = Array.from(sideTimelineYears);
+sideTimeline();
 
-function deactivateYears() {
-    yearsArray.forEach(yearElement => {
-        yearElement.classList.remove('active');
+function sideTimeline() {
+    const blocks = document.querySelectorAll('[data-year]');
+    const sideTimelineYears = document.querySelectorAll('nav li');
+    const yearsArray = Array.from(sideTimelineYears);
+
+    function deactivateYears() {
+        yearsArray.forEach(yearElement => {
+            yearElement.classList.remove('active');
+        })
+    }
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const year = entry.target.dataset.year;
+                const sideTimelineYear = yearsArray.find(yearElement => yearElement.className.includes(year));
+
+                if (sideTimelineYear) {
+                    deactivateYears();
+                    sideTimelineYear.classList.add('active');
+                }
+            }
+        })
+    },
+        {
+            threshold: .25,
+        })
+
+    blocks.forEach(block => {
+        observer.observe(block);
     })
 }
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            const year = entry.target.dataset.year;
-            const sideTimelineYear = yearsArray.find(yearElement => yearElement.className.includes(year));
-            
-            if (sideTimelineYear) {
-                deactivateYears();
-                sideTimelineYear.classList.add('active');
-            }
-        }
-    })
-},
-{
-    threshold: .25,
-})
 
-blocks.forEach(block => {
-    observer.observe(block);
-})
